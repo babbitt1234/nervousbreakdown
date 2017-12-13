fb882879608c7a8b5ebf6de31bfa60ee909f51a6
Satoshi Tezuka <mest_mest_mest@hotmail.com>
2017-12-14 01:26:55 +0900

    2017/12/14/1:26

--- nb.js
+++ nb.js
@@ -1,47 +1,20 @@
 $(function(){
-    
-    var arr = [];
-    $('ul li').each(function() {
-        arr.push($(this).html());
-    });
-    arr.sort(function() {
-        return Math.random() - Math.random();
-    });
-    $('ul').empty();
-    for(i = 0; i < arr.length; i++) {
-        $('ul').append('<li class="card card' + arr[i] + '">' + arr[i] + '</li>');
-    }
-        
 
-    $('.card').click(function(){
-        $('li').css('backgroundColor', 'white');
-    });
-    
-    
-    
-    
-    
-    
-    
-//----------------------ゴミ箱---------------------------
-//    function shuffle(){
-//        var box = [];
-//        $('.card').each(function() {
-//            box.push($(this).html());
-//        });
-//        box.sort(function() {
-//            return Math.random() - Math.random();
-//        });
-//        $('.card').empty();
-//         
-//        i = 0;
-//        $('.card').each(function() {
-//            $(this).append(box[i]);
-//            i++;
-//        });
-//    }
-//    shuffle();    
-    
-    
-    
+    function shuffle(){
+        var arr = [];
+        $('#cards .card').each(function() {
+            arr.push($(this).html());
+        });
+        arr.sort(function() {
+            return Math.random() - Math.random();
+        });
+        $('#cards').empty();
+        for(i = 0; i < arr.length; i++) {
+
+             $('#cards').append('<div class="cardOuter"><div class="cardInner">' +'<div class="card">'+ arr[i] + '</div>' + '<div class="cardBack"><img src="card.jpg" alt=""></div></div></div>');
+        }
+//        $('.card').css('visibility','hidden');
+    }
+    shuffle();
+
 });
