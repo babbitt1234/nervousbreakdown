6d243881593f404b64478f29cc67d14fcb1091e6
Satoshi Tezuka <mest_mest_mest@hotmail.com>
2017-12-14 20:16:24 +0900

    2017/12/14/20:16

--- nb.js
+++ nb.js
@@ -13,8 +13,59 @@ $(function(){
 
              $('#cards').append('<div class="cardOuter"><div class="cardInner">' +'<div class="card">'+ arr[i] + '</div>' + '<div class="cardBack"><img src="card.jpg" alt=""></div></div></div>');
         }
-//        $('.card').css('visibility','hidden');
     }
     shuffle();
+    
+    $.fn.rotate_box = function(){
+        var	elm = $(this),
+            elm_in = $('.cardInner', this),
+            btn = $('.card, .cardBack', elm),
+            deg = 0,
+            turn = false,
+            turn_cls = 'reverse';
 
+    var rotate_anm = function(){
+        elm_in.css({
+            'transform' : 'rotateY(' + deg * -2 + 'deg)'
+        });
+    };
+
+        var rotate = function(){
+            setTimeout(function(){
+                rotate_anm();
+                if( deg == 45 ){
+                    if( turn === false ){
+                        elm.addClass(turn_cls);
+                    } else {
+                        elm.removeClass(turn_cls);
+                    }
+                    deg = 315;
+                }
+                if( deg <= 45 ){
+                    deg += 3;
+                    rotate();
+                } else if( deg < 360 && deg > 45 ) {
+                    deg += 3;
+                    rotate();
+                } else {
+                    deg = 0;
+                    elm_in.attr('style', '');
+                    if( turn === false ){
+                        turn = true;
+                    } else {
+                        turn = false;
+                    }
+                }
+            }, 5);
+        };
+
+        btn.click(function(){
+            rotate();
+        });
+    };
+
+    $('.cardOuter').each(function(){
+        $(this).rotate_box();
+    });
+   
 });
